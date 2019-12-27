3ab253b7aedfc7c5d6ed30c100261d08ddd46fa9
unknown <jodysetiawan1570@gmail.com>
2019-12-27 22:13:19 +0700

    json 3 level depth

---
+++ object.js
@@ -0,0 +1,25 @@
+laptop = {
+    "brand" : "MSI",//level 1
+    "produk" : {
+        "nama" : "MSI GT73VR 7RF TITAN PRO",//level 2
+        "cpu" : "Core i7",
+        "gpu" : "GeForce GTX 1080",
+        "warna" : ["hitam","coklat","kuning"],
+        "harga" : "gratis",
+        "spesial" : { //level 3
+            "pengiriman" : "jne",
+            "packing" : "dibungkus 2x",
+            "jenis" : "paket kilat"
+        }
+    },
+    "pembeli" : "m jody setiawan",
+    "alamat" : {
+        "kota" : "surabaya",
+        "kecamatan" : "karang pilang",
+        "kelurahan" : "kebraon",
+        "description" : ["dekat pohon mangga","cat warna hitam","ada kucingnya"]
+    },
+    "pembayaran" : "Pos polisi terdekat",
+    "order" : "12-12-2025"
+}
+ console.log (laptop)
